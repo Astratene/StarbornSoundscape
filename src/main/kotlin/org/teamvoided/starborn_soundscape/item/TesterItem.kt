482fd918bc2b0b198a6f74a8f1deb3c92b88573f
Mxz-Astatine <davebobsos910@gmail.com>
2026-02-06 17:44:52 +0000

    one tiny touch up

--- src/main/kotlin/org/teamvoided/starborn_soundscape/item/TesterItem.kt
+++ src/main/kotlin/org/teamvoided/starborn_soundscape/item/TesterItem.kt
@@ -4,6 +4,8 @@ import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.Item
 import net.minecraft.item.Item.Settings
 import net.minecraft.item.ItemStack
+import net.minecraft.particle.ParticleTypes
+import net.minecraft.server.world.ServerWorld
 import net.minecraft.util.Hand
 import net.minecraft.util.TypedActionResult
 import net.minecraft.util.math.Vec3d
@@ -13,6 +15,20 @@ import org.teamvoided.starborn_soundscape.entity.SmallSpeakerEntity
 class TesterItem(settings: Settings) : Item(settings) {
 
     override fun use(world: World, user: PlayerEntity, hand: Hand?): TypedActionResult<ItemStack?>? {
+        if (!world.isClient) {
+            val serverWorld = world as ServerWorld
+            serverWorld.spawnParticles(
+                ParticleTypes.GLOW,
+                user.x,
+                user.y + 1,
+                user.z,
+                50,
+                1.0,
+                2.0,
+                1.0,
+                0.0
+            )
+        }
         val positions =
             mutableListOf(Vec3d(1.0, 2.5, -0.5),
                 Vec3d(-1.0, 2.5, -0.5),
