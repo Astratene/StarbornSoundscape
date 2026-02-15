f7acfaad6bcc7e56d312eaf2d4570d246dd294f0
Mxz-Astatine <davebobsos910@gmail.com>
2026-02-15 22:45:33 +0000

    Fixed speakers crashing the game when prioritising players

--- src/main/kotlin/org/teamvoided/starborn_soundscape/entity/SmallSpeakerEntity.kt
+++ src/main/kotlin/org/teamvoided/starborn_soundscape/entity/SmallSpeakerEntity.kt
@@ -227,6 +227,7 @@ class SmallSpeakerEntity : Entity, GeoEntity {
                             entities.remove(entiity)
                         }
                     }
+                    break
                 }
             }
             entities.shuffle()
